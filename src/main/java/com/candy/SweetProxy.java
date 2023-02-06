a152a3b06bbe0fc37890dec56921c11348eb1d12
Egor Tunekov <69630128+olovink@users.noreply.github.com>
2023-02-06 22:03:35 +0300

    update

--- src/main/java/com/candy/SweetProxy.java
+++ src/main/java/com/candy/SweetProxy.java
@@ -2,13 +2,18 @@ package com.candy;
 
 
 import com.candy.utils.SweetUtils;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+
+
 @Log4j2
+@Getter
 public class SweetProxy {
 
+    public static final long START_TIME = System.currentTimeMillis();
     private final AtomicBoolean running = new AtomicBoolean(true);
 
     public static void main(String[] args) {
@@ -19,8 +24,30 @@ public class SweetProxy {
 
     public void enableProxy() {
         running.set(true);
-        if (running.get()) {
-            log.info("Proxy enabled on 0.0.0.0");
+        log.info("Proxy enabled on 0.0.0.0");
+        log.info("Server startup took " + (double) (System.currentTimeMillis() - START_TIME) / 1000 + " seconds");
+
+        run();
+    }
+
+    private void run() {
+        while (running.get()) {
+            try {
+                synchronized (this) {
+                    this.wait();
+                }
+            } catch (InterruptedException exception) {
+                // ignore
+            }
+
+        }
+
+    }
+    public void shutdown() {
+        if (running.compareAndSet(true, false)) {
+            synchronized (this) {
+                this.notify();
+            }
         }
     }
 }
